46bcd8e79d3b4781008b1a7fcfb92c2e427245d0
Ayan Srivastava <ayansrivastava322@gmail.com>
2025-04-30 23:54:53 +0530

    initial commit

---
+++ basic/Nth Term of AP/main.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,a,d;
+    cin>>n>>a>>d;
+    int Tn = a + (n-1)*d;
+    cout<<Tn;
+
+    return 0;
+}
